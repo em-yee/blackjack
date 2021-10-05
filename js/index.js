bbf2d5a9dda50c2b48912f7dbf47e8ea00abe8f4
Emily Ye <emily.ye.dev@gmail.com>
2021-10-05 09:52:48 -0400

    finishes blackjack game

--- js/index.js
+++ js/index.js
@@ -21,19 +21,19 @@ const dealerCardValB = document.getElementById('dealer-card-val-b');
 
 const cardSuits = ['Diamonds', 'Clubs', 'Hearts', 'Spades'];
 const cardValues = [
-	'Two',
-	'Three',
-	'Four',
-	'Five',
-	'Six',
-	'Seven',
-	'Eight',
-	'Nine',
-	'Ten',
-	'Jack',
-	'Queen',
-	'King',
-	'Ace',
+  'Two',
+  'Three',
+  'Four',
+  'Five',
+  'Six',
+  'Seven',
+  'Eight',
+  'Nine',
+  'Ten',
+  'Jack',
+  'Queen',
+  'King',
+  'Ace',
 ];
 
 buttonContainer.style.display = 'none';
@@ -45,317 +45,331 @@ let gameOver = false;
 let playerWins = false;
 
 let dealer = {
-	cards: [],
-	score: 0,
+  cards: [],
+  score: 0,
 };
 
 let player = {
-	cards: [],
-	score: 0,
+  cards: [],
+  score: 0,
 };
 
 newGameButton.addEventListener('click', function () {
-	newGameButton.style.display = 'none';
-	buttonContainer.style.display = 'flex';
-	winnerContainer.style.display = 'none';
-	cardsContainer.style.display = 'flex';
+  newGameButton.style.display = 'none';
+  buttonContainer.style.display = 'flex';
+  winnerContainer.style.display = 'none';
+  cardsContainer.style.display = 'flex';
+  textArea.innerText = '';
 
-	started = true;
-	winnerText = '';
+  started = true;
+  winnerText = '';
 
-	deck = createNewDeck();
-	shuffleDeck(deck);
+  deck = createNewDeck();
+  shuffleDeck(deck);
 
-	dealer.cards = [handleDealCard(), handleDealCard()];
-	player.cards = [handleDealCard(), handleDealCard()];
+  dealer.cards = [handleDealCard(), handleDealCard()];
+  player.cards = [handleDealCard(), handleDealCard()];
 
-	handleUpdateScores();
+  handleUpdateScores();
+  const newCards = document.querySelectorAll('div#new-cards');
 
-	const newCards = document.querySelectorAll('div#new-cards');
-	// console.log(newCards);
-
-	newCards.forEach(function (el) {
-		el.remove();
-	});
-
-	console.log(player.cards);
+  newCards.forEach(function (el) {
+    el.remove();
+  });
 });
 
 hitButton.addEventListener('click', function () {
-	handlePlayerHit();
-	handleUpdateScores();
-	handleCheckGameStatus();
+  handlePlayerHit();
+  handleUpdateScores();
+  handleCheckGameStatus();
+  // handleRenderImages();
 });
 
 stayButton.addEventListener('click', function () {
-	gameOver = true;
-	handleCheckGameStatus();
+  gameOver = true;
+  handleCheckGameStatus();
+  // handleRenderImages();
 });
 
 const handlePlayerHit = () => {
-	player.cards.push(handleDealCard());
+  player.cards.push(handleDealCard());
 };
 
 const handleFindImage = (card) => {
-	switch (card.value) {
-		case 'Jack':
-			return `../assets/${card.suit.toLowerCase()}/11.png`;
-		case 'Queen':
-			return `../assets/${card.suit.toLowerCase()}/12.png`;
-		case 'King':
-			return `../assets/${card.suit.toLowerCase()}/13.png`;
-		default:
-			return `../assets/${card.suit.toLowerCase()}/${getCardValInt(
-				card.value
-			)}.png`;
-	}
+  switch (card.value) {
+    case 'Jack':
+      return `../assets/${card.suit.toLowerCase()}/11.png`;
+    case 'Queen':
+      return `../assets/${card.suit.toLowerCase()}/12.png`;
+    case 'King':
+      return `../assets/${card.suit.toLowerCase()}/13.png`;
+    default:
+      return `../assets/${card.suit.toLowerCase()}/${getCardValInt(
+        card.value
+      )}.png`;
+  }
 };
 
 const createNewDeck = () => {
-	let deck = [];
-	for (let i = 0; i < cardSuits.length; i++) {
-		for (let j = 0; j < cardValues.length; j++) {
-			let card = {
-				suit: cardSuits[i],
-				value: cardValues[j],
-			};
-			deck.push(card);
-		}
-	}
+  let deck = [];
+  for (let i = 0; i < cardSuits.length; i++) {
+    for (let j = 0; j < cardValues.length; j++) {
+      let card = {
+        suit: cardSuits[i],
+        value: cardValues[j],
+      };
+      deck.push(card);
+    }
+  }
 
-	return deck;
+  return deck;
 };
 
 const shuffleDeck = (deck) => {
-	for (let i = 0; i < deck.length; i++) {
-		// selects a random number less than deck.length
-		let randomIndex = Math.trunc(Math.random() * deck.length);
+  for (let i = 0; i < deck.length; i++) {
+    // selects a random number less than deck.length
+    let randomIndex = Math.trunc(Math.random() * deck.length);
 
-		// assigns a random card to a temporary placeholder variable
-		let tempCard = deck[randomIndex];
+    // assigns a random card to a temporary placeholder variable
+    let tempCard = deck[randomIndex];
 
-		// reassigns the same card to current value of "i" in loop
-		deck[randomIndex] = deck[i];
+    // reassigns the same card to current value of "i" in loop
+    deck[randomIndex] = deck[i];
 
-		// assigns, in order, randomly generated card from line 70
-		deck[i] = tempCard;
-	}
+    // assigns, in order, randomly generated card from line 70
+    deck[i] = tempCard;
+  }
 };
 
 const getCardValInt = (cardVal) => {
-	switch (cardVal) {
-		case 'Ace':
-			return 1;
-		case 'Two':
-			return 2;
-		case 'Three':
-			return 3;
-		case 'Four':
-			return 4;
-		case 'Five':
-			return 5;
-		case 'Six':
-			return 6;
-		case 'Seven':
-			return 7;
-		case 'Eight':
-			return 8;
-		case 'Nine':
-			return 9;
-		default:
-			return 10;
-	}
+  switch (cardVal) {
+    case 'Ace':
+      return 1;
+    case 'Two':
+      return 2;
+    case 'Three':
+      return 3;
+    case 'Four':
+      return 4;
+    case 'Five':
+      return 5;
+    case 'Six':
+      return 6;
+    case 'Seven':
+      return 7;
+    case 'Eight':
+      return 8;
+    case 'Nine':
+      return 9;
+    default:
+      return 10;
+  }
 };
 
 const handleDealCard = () => {
-	return deck.shift();
+  return deck.shift();
 };
 
 const handleCheckGameStatus = () => {
-	playerScore.style.display = 'block';
-	dealerScore.style.display = 'block';
+  playerScore.style.display = 'block';
+  dealerScore.style.display = 'block';
 
-	handleRenderNewCards();
-	handleUpdateScores();
+  handleUpdateScores();
+  handleRenderNewCards();
+  // console.log(gameOver);
 
-	if (gameOver) {
-		while (
-			dealer.score < player.score &&
-			player.score <= 21 &&
-			dealer.score <= 21
-		) {
-			dealer.cards.push(handleDealCard());
-			handleRenderNewCards();
-			handleUpdateScores();
-		}
-	}
+  if (gameOver) {
+    // console.log(
+    //   dealer.score < player.score && player.score <= 21 && dealer.score <= 21
+    // );
+    while (
+      dealer.score < player.score &&
+      player.score <= 21 &&
+      dealer.score <= 21
+    ) {
+      dealer.cards.push(handleDealCard());
+      handleUpdateScores();
+    }
+  }
 
-	if (player.score > 21) {
-		playerWins = false;
-		// winnerText = 'Dealer Wins!';
-		gameOver = true;
-	} else if (dealer.score > 21) {
-		// winnerText = 'Player Wins!';
-		playerWins = true;
-		gameOver = true;
-	} else if (gameOver) {
-		winnerContainer.innerText = winnerText;
-		winnerContainer.style.display = 'block';
-		buttonContainer.style.display = 'none';
-		if (player.score > dealer.score) {
-			playerWins = true;
-		} else if (dealer.score > player.score) {
-			playerWins = false;
-		}
-	}
+  if (player.score > 21) {
+    playerWins = false;
+    gameOver = true;
+    handleWinnerAlert();
+    // alert("Boooo, dealer wins.");
+  } else if (dealer.score > 21) {
+    playerWins = true;
+    gameOver = true;
+    handleWinnerAlert();
+    // alert("Congrats!! Player Wins!");
+  } else if (gameOver) {
+    // buttonContainer.style.display = 'none';
+    if (player.score > dealer.score) {
+      playerWins = true;
+      handleWinnerAlert();
+    } else {
+      playerWins = false;
+      handleWinnerAlert();
+    }
+  }
 
-	winnerContainer.innerText = winnerText;
-	winnerContainer.style.display = 'block';
-	newGameButton.style.display = 'block';
+  newGameButton.style.display = 'block';
+};
+
+const handleWinnerAlert = () => {
+  if (gameOver) {
+    if (playerWins) {
+      textArea.innerText = 'Congrats!! Player Wins!';
+    } else if (!playerWins) {
+      textArea.innerText = 'Boooo, dealer wins.';
+    } else {
+      textArea.innerText = "Hey, it's a tie!";
+    }
+  }
 };
 
 const handleCalcHandScore = (hand) => {
-	let score = 0;
-	let isAce = false;
+  let score = 0;
+  let isAce = false;
 
-	handleRenderImages();
+  handleRenderImages();
 
-	for (let i = 0; i < hand.length; i++) {
-		let card = hand[i];
-		isAce = false;
-		score += getCardValInt(card.value);
+  for (let i = 0; i < hand.length; i++) {
+    let card = hand[i];
+    isAce = false;
+    score += getCardValInt(card.value);
 
-		if (card.value === 'Ace') {
-			isAce = true;
-		}
-	}
-	if (isAce && score + 10 <= 21) {
-		return score + 10;
-	} else {
-		return score;
-	}
+    if (card.value === 'Ace') {
+      isAce = true;
+    }
+  }
+  if (isAce && score + 10 <= 21) {
+    return score + 10;
+  } else {
+    return score;
+  }
 };
 
 const handleSuitIcon = (suit) => {
-	let suitIcon = '';
+  let suitIcon = '';
 
-	switch (suit) {
-		case 'Spades':
-			suitIcon = '♠';
-			break;
-		case 'Hearts':
-			suitIcon = '♥';
-			break;
-		case 'Clubs':
-			suitIcon = '♣';
-			break;
-		case 'Diamonds':
-			suitIcon = '♦';
-			break;
-		default:
-	}
+  switch (suit) {
+    case 'Spades':
+      suitIcon = '♠';
+      break;
+    case 'Hearts':
+      suitIcon = '♥';
+      break;
+    case 'Clubs':
+      suitIcon = '♣';
+      break;
+    case 'Diamonds':
+      suitIcon = '♦';
+      break;
+    default:
+  }
 
-	return suitIcon;
+  return suitIcon;
 };
 
 const handleRenderImages = () => {
-	playerCardA.src = handleFindImage(player.cards[0]);
-	playerCardB.src = handleFindImage(player.cards[1]);
+  playerCardA.src = handleFindImage(player.cards[0]);
+  playerCardB.src = handleFindImage(player.cards[1]);
 
-	playerCardValA.innerText = `${handleRenderOverlayContent(
-		player.cards[0].value
-	)} of ${handleSuitIcon(player.cards[0].suit)}`;
-	playerCardValB.innerText = `${handleRenderOverlayContent(
-		player.cards[1].value
-	)} of ${handleSuitIcon(player.cards[1].suit)}`;
+  playerCardValA.innerText = `${handleRenderOverlayContent(
+    player.cards[0].value
+  )} of ${handleSuitIcon(player.cards[0].suit)}`;
+  playerCardValB.innerText = `${handleRenderOverlayContent(
+    player.cards[1].value
+  )} of ${handleSuitIcon(player.cards[1].suit)}`;
 
-	dealerCardA.src = handleFindImage(dealer.cards[0]);
-	dealerCardB.src = handleFindImage(dealer.cards[1]);
+  dealerCardA.src = handleFindImage(dealer.cards[0]);
+  dealerCardB.src = handleFindImage(dealer.cards[1]);
 
-	dealerCardValA.innerText = `${handleRenderOverlayContent(
-		dealer.cards[0].value
-	)} of ${handleSuitIcon(dealer.cards[0].suit)}`;
-	dealerCardValB.innerText = `${handleRenderOverlayContent(
-		dealer.cards[1].value
-	)} of ${handleSuitIcon(dealer.cards[1].suit)}`;
+  dealerCardValA.innerText = `${handleRenderOverlayContent(
+    dealer.cards[0].value
+  )} of ${handleSuitIcon(dealer.cards[0].suit)}`;
+  dealerCardValB.innerText = `${handleRenderOverlayContent(
+    dealer.cards[1].value
+  )} of ${handleSuitIcon(dealer.cards[1].suit)}`;
 };
 
 const handleRenderOverlayContent = (card) => {
-	switch (card) {
-		case 'Ace':
-			return 'A';
-		case 'Jack':
-			return `J`;
-		case 'Queen':
-			return `Q`;
-		case 'King':
-			return `K`;
-		default:
-			return getCardValInt(card);
-	}
+  switch (card) {
+    case 'Ace':
+      return 'A';
+    case 'Jack':
+      return `J`;
+    case 'Queen':
+      return `Q`;
+    case 'King':
+      return `K`;
+    default:
+      return getCardValInt(card);
+  }
 };
 
 const handleUpdateScores = () => {
-	dealer.score = handleCalcHandScore(dealer.cards);
-	player.score = handleCalcHandScore(player.cards);
-	console.log(player.cards);
+  dealer.score = handleCalcHandScore(dealer.cards);
+  player.score = handleCalcHandScore(player.cards);
 
-	dealerScore.innerText = dealer.score;
-	playerScore.innerText = player.score;
+  dealerScore.innerText = dealer.score;
+  playerScore.innerText = player.score;
 
-	cardsContainer.style.display = 'flex';
+  // handleWinnerAlert();
+
+  // handleRenderNewCards();
+  console.log(player.score, dealer.score);
+
+  cardsContainer.style.display = 'flex';
 };
 
 const handleRenderNewCards = () => {
-	let newCardWrapper = document.createElement('div');
-	let newCardImage = document.createElement('img');
-	let newCardOverlay = document.createElement('span');
+  let newCardWrapper = document.createElement('div');
+  let newCardImage = document.createElement('img');
+  let newCardOverlay = document.createElement('span');
 
-	newCardWrapper.classList.add('card-wrapper');
-	newCardWrapper.setAttribute('id', 'new-cards');
+  newCardWrapper.classList.add('card-wrapper');
+  newCardWrapper.setAttribute('id', 'new-cards');
 
-	if (gameOver) {
-		newCardImage.setAttribute(
-			'src',
-			handleFindImage(dealer.cards[dealer.cards.length - 1])
-		);
-		newCardImage.setAttribute(
-			'alt',
-			`${getCardValInt(
-				dealer.cards[dealer.cards.length - 1].value
-			)} of ${handleSuitIcon(dealer.cards[dealer.cards.length - 1].suit)}`
-		);
+  if (gameOver && playerWins) {
+    newCardImage.setAttribute(
+      'src',
+      handleFindImage(dealer.cards[dealer.cards.length - 1])
+    );
+    newCardImage.setAttribute(
+      'alt',
+      `${getCardValInt(
+        dealer.cards[dealer.cards.length - 1].value
+      )} of ${handleSuitIcon(dealer.cards[dealer.cards.length - 1].suit)}`
+    );
 
-		newCardOverlay.classList.add('hover');
-		newCardOverlay.innerText = `${getCardValInt(
-			dealer.cards[dealer.cards.length - 1].value
-		)} of ${handleSuitIcon(dealer.cards[dealer.cards.length - 1].suit)}`;
+    newCardOverlay.classList.add('hover');
+    newCardOverlay.innerText = `${getCardValInt(
+      dealer.cards[dealer.cards.length - 1].value
+    )} of ${handleSuitIcon(dealer.cards[dealer.cards.length - 1].suit)}`;
 
-		newCardWrapper.appendChild(newCardImage);
-		newCardWrapper.appendChild(newCardOverlay);
-		dealerHand.appendChild(newCardWrapper);
-    console.log(dealerHand);
-	} else {
-		newCardImage.setAttribute(
-			'src',
-			handleFindImage(player.cards[player.cards.length - 1])
-		);
-		newCardImage.setAttribute(
-			'alt',
-			`${getCardValInt(
-				player.cards[player.cards.length - 1].value
-			)} of ${handleSuitIcon(player.cards[player.cards.length - 1].suit)}`
-		);
-	}
+    newCardWrapper.appendChild(newCardImage);
+    newCardWrapper.appendChild(newCardOverlay);
+    dealerHand.appendChild(newCardWrapper);
+  } else {
+    newCardImage.setAttribute(
+      'src',
+      handleFindImage(player.cards[player.cards.length - 1])
+    );
+    newCardImage.setAttribute(
+      'alt',
+      `${getCardValInt(
+        player.cards[player.cards.length - 1].value
+      )} of ${handleSuitIcon(player.cards[player.cards.length - 1].suit)}`
+    );
+    newCardOverlay.classList.add('hover');
+    newCardOverlay.innerText = `${getCardValInt(
+      player.cards[player.cards.length - 1].value
+    )} of ${handleSuitIcon(player.cards[player.cards.length - 1].suit)}`;
 
-	newCardOverlay.classList.add('hover');
-	newCardOverlay.innerText = `${getCardValInt(
-		player.cards[player.cards.length - 1].value
-	)} of ${handleSuitIcon(player.cards[player.cards.length - 1].suit)}`;
-
-	newCardWrapper.appendChild(newCardImage);
-	newCardWrapper.appendChild(newCardOverlay);
-	dealerHand.appendChild(newCardWrapper);
-  console.log(playerHand)
-
+    newCardWrapper.appendChild(newCardImage);
+    newCardWrapper.appendChild(newCardOverlay);
+    playerHand.appendChild(newCardWrapper);
+  }
 };
